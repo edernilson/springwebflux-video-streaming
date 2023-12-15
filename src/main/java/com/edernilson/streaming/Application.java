b722965976c7ba6794ddf4a3e09fbcecb89cb85c
Eder Nilson <eder.nilson@pathfind.com.br>
2023-12-15 13:21:23 -0300

    Initial commit

---
+++ src/main/java/com/edernilson/streaming/Application.java
@@ -0,0 +1,13 @@
+package com.edernilson.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxVideoStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebfluxVideoStreamingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/edernilson/streaming/StreamingService.java
@@ -0,0 +1,9 @@
+package com.edernilson.streaming;
+
+/**
+ * @author: github.com/edernilson
+ * @user: eder.nilson
+ * @created: 15/12/2023, sexta-feira
+ */
+public class StreamingService {
+}
